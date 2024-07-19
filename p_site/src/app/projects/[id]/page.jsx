901ed5902930d2ee4ad269ee351b2bdac97d0bc8
aneeshpatne <aneeshpatne@gmail.com>
2024-07-19 16:03:53 +0530

    removed old project

--- p_site/src/app/projects/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext } from "../../../../componenets/ThemeProvider";
-export default function Project({params}){ 
-    const {theme, toggleTheme } = useContext(ThemeContext);
-    return(<div>Hello!
-        {params.id}
-    </div>);
-}
